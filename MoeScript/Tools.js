03975c321d529acf4556b802454a15d217941c46
ggg555ttt <lxy19950401@126.com>
2023-10-13 19:35:03 +0800

    2.8

--- MoeScript/Tools.js
+++ MoeScript/Tools.js
@@ -310,13 +310,10 @@ function blobToArrayBuffer(file) {
 }
 $('body').on('click',"#mt-edit",function()
 {
-	if(confirm('是否切换编辑模式？\n点击【确定】为将使用新版编辑模式\n点击【取消】将使用旧版编辑模式'))
+	localStorage.removeItem('mt-edit')
+	if(confirm('此功能已被弃用，旧版的追加功能已被整合进新版的消息编辑功能内\n点击确定可以跳转旧版MoeTalk使用旧版编辑功能'))
 	{
-		localStorage.removeItem('mt-edit');
-	}
-	else
-	{
-		localStorage['mt-edit'] = '旧版编辑模式';
+		window.location.replace('https://ggg555ttt.gitee.io/mothertalk/')
 	}
 })
 $('body').on('click',"#ct",function()
